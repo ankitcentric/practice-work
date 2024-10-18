6ddb426e9e214187d336ca8d3643762edc30163f
ankitcentric <ankit.sharma2@centricconsulting.com>
2024-10-18 11:52:41 +0530

    Adding second program

--- gitdemo/src/test/java/com/centric/Chapter1.java
+++ gitdemo/src/test/java/com/centric/Chapter1.java
@@ -2,4 +2,5 @@ package com.centric;
 
 public class Chapter1 {
     // this is a comment
+    System.out.println("centric");
 }
---
+++ gitdemo/src/test/java/com/centric/second.java
@@ -0,0 +1,5 @@
+package com.centric;
+
+public class second {
+    
+}
